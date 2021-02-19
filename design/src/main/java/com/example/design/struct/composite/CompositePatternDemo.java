92236f8ae196ca50b48f6f749fb3a5472bb80a38
‘wul’ <279359180@qq.com>
2021-02-19 10:29:34 +0800

    #009 组合模式

---
+++ design/src/main/java/com/example/design/struct/composite/CompositePatternDemo.java
@@ -0,0 +1,39 @@
+package com.example.design.struct.composite;
+
+public class CompositePatternDemo {
+
+    public static void main(String[] args) {
+        Employee CEO = new Employee("John","CEO", 30000);
+
+        Employee headSales = new Employee("Robert","Head Sales", 20000);
+
+        Employee headMarketing = new Employee("Michel","Head Marketing", 20000);
+
+        Employee clerk1 = new Employee("Laura","Marketing", 10000);
+        Employee clerk2 = new Employee("Bob","Marketing", 10000);
+
+        Employee salesExecutive1 = new Employee("Richard","Sales", 10000);
+        Employee salesExecutive2 = new Employee("Rob","Sales", 10000);
+
+        CEO.add(headSales);
+        CEO.add(headMarketing);
+
+        headSales.add(salesExecutive1);
+        headSales.add(salesExecutive2);
+
+        headMarketing.add(clerk1);
+        headMarketing.add(clerk2);
+
+        print(CEO);
+
+    }
+
+    private static void print(Employee employee) {
+        if (employee !=null) {
+            System.out.println(employee);
+            if (employee.getSubordinates().size() > 0) {
+                employee.getSubordinates().forEach(e -> print(e));
+            }
+        }
+    }
+}
