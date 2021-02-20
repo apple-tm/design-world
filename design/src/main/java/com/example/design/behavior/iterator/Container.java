54a65bd4e05d9f504c6440951513583ef498ca2b
‘wul’ <279359180@qq.com>
2021-02-20 15:48:51 +0800

    #016 迭代器模式

---
+++ design/src/main/java/com/example/design/behavior/iterator/Container.java
@@ -0,0 +1,5 @@
+package com.example.design.behavior.iterator;
+
+public interface Container {
+    Iterator getIterator();
+}
---
+++ design/src/main/java/com/example/design/behavior/iterator/Iterator.java
@@ -0,0 +1,6 @@
+package com.example.design.behavior.iterator;
+
+public interface Iterator {
+    boolean hasNext();
+    Object next();
+}
