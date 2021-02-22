1b70405befbd9fe25a5a28f0d00e9be40c9aa528
‘wul’ <279359180@qq.com>
2021-02-22 09:56:09 +0800

    #017 状态模式

--- design/src/main/java/com/example/design/behavior/observer/ObserverPatternDemo.java
+++ design/src/main/java/com/example/design/behavior/observer/ObserverPatternDemo.java
@@ -1,5 +1,11 @@
 package com.example.design.behavior.observer;
 
+/**
+ * Observe
+ *
+ * 1.通知机制
+ * 2.java 有观察者模式实现模板
+ */
 public class ObserverPatternDemo {
     public static void main(String[] args) {
         Subject subject = new Subject();
