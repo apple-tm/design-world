19386f36d5d103cf63ed3d1098ab6349df5df942
‘wul’ <279359180@qq.com>
2021-02-22 09:56:34 +0800

    #017 状态模式

---
+++ design/src/main/java/com/example/design/behavior/state/State.java
@@ -0,0 +1,5 @@
+package com.example.design.behavior.state;
+
+public interface State {
+    void doAction(Context context);
+}
---
+++ design/src/main/java/com/example/design/behavior/state/StatePatternDemo.java
@@ -0,0 +1,22 @@
+package com.example.design.behavior.state;
+
+/**
+ * state 模式
+ *
+ * 1.
+ */
+public class StatePatternDemo {
+    public static void main(String[] args) {
+        Context context = new Context();
+
+        StartState startState = new StartState();
+        startState.doAction(context);
+
+        System.out.println(context.getState().toString());
+
+        StopState stopState = new StopState();
+        stopState.doAction(context);
+
+        System.out.println(context.getState().toString());
+    }
+}
