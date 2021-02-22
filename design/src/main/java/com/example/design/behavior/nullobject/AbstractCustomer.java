19b2b8069a1280e229b40923e0011896be55b19c
‘wul’ <279359180@qq.com>
2021-02-22 10:15:09 +0800

    #018 空对象模式

---
+++ design/src/main/java/com/example/design/behavior/nullobject/AbstractCustomer.java
@@ -0,0 +1,7 @@
+package com.example.design.behavior.nullobject;
+
+public abstract class AbstractCustomer {
+    protected String name;
+    public abstract boolean isNil();
+    public abstract String getName();
+}
