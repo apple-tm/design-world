05d047922c3334921cb29c25add23cae635970f5
‘wul’ <279359180@qq.com>
2021-02-22 14:57:31 +0800

    #024 组合实体模式

---
+++ design/src/main/java/com/example/design/web/compositeentity/DependentObject1.java
@@ -0,0 +1,13 @@
+package com.example.design.web.compositeentity;
+
+public class DependentObject1 {
+    private String data;
+
+    public void setData(String data){
+        this.data = data;
+    }
+
+    public String getData(){
+        return data;
+    }
+}
